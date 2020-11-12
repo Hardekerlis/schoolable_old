e3b796641c2b60f5d51b6fc720f5abcf37793ea7
Gustaf Dahl <gustaf-dahl@telia.com>
2020-11-12 20:18:25 +0100

    added missed export for NotFoundError class in common module

--- common/src/errors/not-found.ts
+++ common/src/errors/not-found.ts
@@ -1,6 +1,6 @@
 import { CustomError } from './custom-error';
 
-class NotFoundError extends CustomError {
+export class NotFoundError extends CustomError {
   statusCode = 404;
 
   constructor() {
