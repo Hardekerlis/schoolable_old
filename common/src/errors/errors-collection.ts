ac49892097dcee838bab8a7c9ff2f55d46fb60d8
Gustaf Dahl <gustaf-dahl@telia.com>
2020-11-15 17:14:39 +0100

    idc

--- common/src/errors/errors-collection.ts
+++ common/src/errors/errors-collection.ts
@@ -2,3 +2,4 @@ export * from './database-connection';
 export * from './not-authorized';
 export * from './not-found';
 export * from './request-validation';
+export * from './bad-request';
---
+++ common/src/events/subjects.ts
@@ -0,0 +1,7 @@
+export enum Subjects {
+  AccountCreated = 'account:created',
+  AccountUpdated = 'account:updated',
+  AccountDeleted = 'account:deleted'
+
+  
+}
