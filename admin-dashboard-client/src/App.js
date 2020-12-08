020b419ecb8463917d9c3dcc92168e37f26e25fa
Gustaf Dahl <gustaf-dahl@telia.com>
2020-12-08 18:16:30 +0100

    admin-dashboard-client is ready for development

--- admin-dashboard-client/src/App.js
+++ admin-dashboard-client/src/App.js
@@ -4,6 +4,9 @@ import React from 'react';
 import { Admin } from 'react-admin';
 
 function App() {
+	console.log(
+		'Go to https://github.com/Hardekerlis/reactAdminExample for reference',
+	);
 	return <div></div>;
 }
 
